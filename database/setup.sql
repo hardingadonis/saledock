b3ab3c1757e1ffb754169c94c8b175ee6d180a8a
Thunder <htn10a2@gmail.com>
2024-01-08 09:39:49 +0700

    Update database UML and code SQL

--- database/setup.sql
+++ database/setup.sql
@@ -1,60 +1,63 @@
 CREATE TABLE `customer` (
-  `id` VARCHAR(255) PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `address` VARCHAR(255) NOT NULL,
-  `fax` VARCHAR(20) NOT NULL,
+  `id` int PRIMARY KEY AUTO_INCREMENT COMMENT 'ID dùng để quản lý, tự động tăng',
+  `code` varchar(255) UNIQUE COMMENT 'Mã khách hàng, sẽ hiện trên giao diện, ID thì không hiện',
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `created_at` TIMESTAMP DEFAULT null,
   `updated_at` TIMESTAMP DEFAULT null
 );
 
 CREATE TABLE `product` (
-  `id` VARCHAR(255) PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `description` TEXT NOT NULL,
+  `id` int PRIMARY KEY AUTO_INCREMENT COMMENT 'ID dùng để quản lý, tự động tăng',
+  `code` varchar(255) UNIQUE COMMENT 'Mã sản phẩm, sẽ hiện trên giao diện, ID thì không hiện',
+  `name` varchar(255) NOT NULL,
+  `description` TEXT,
   `price` REAL NOT NULL,
-  `category_id` VARCHAR(255) NOT NULL,
+  `category_id` varchar(255) NOT NULL,
   `image_url` LONGTEXT,
   `created_at` TIMESTAMP DEFAULT null,
   `updated_at` TIMESTAMP DEFAULT null
 );
 
 CREATE TABLE `category` (
-  `category_id` VARCHAR(255) PRIMARY KEY,
-  `category_name` VARCHAR(255) NOT NULL
+  `id` int PRIMARY KEY AUTO_INCREMENT COMMENT 'ID dùng để quản lý, tự động tăng',
+  `name` varchar(255) NOT NULL
 );
 
 CREATE TABLE `employee` (
-  `id` VARCHAR(255) PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
+  `id` int PRIMARY KEY AUTO_INCREMENT COMMENT 'ID dùng để quản lý, tự động tăng',
+  `code` varchar(255) UNIQUE COMMENT 'Mã nhân viên, sẽ hiện trên giao diện, dùng để đăng nhập, vd QE170001',
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `created_at` TIMESTAMP DEFAULT null,
   `updated_at` TIMESTAMP DEFAULT null
 );
 
 CREATE TABLE `order` (
-  `order_id` VARCHAR(255) PRIMARY KEY,
-  `employee_id` VARCHAR(255),
-  `customer_id` VARCHAR(255) NOT NULL,
+  `id` int PRIMARY KEY AUTO_INCREMENT COMMENT 'ID dùng để quản lý, tự động tăng',
+  `code` varchar(255) UNIQUE,
+  `employee_id` int NOT NULL,
+  `customer_id` int NOT NULL,
   `status` ENUM ('pending', 'shipping', 'done', 'canceled') NOT NULL,
   `total` REAL NOT NULL,
-  `note` TEXT,
+  `note` TEXT DEFAULT null,
   `created_at` TIMESTAMP DEFAULT null,
   `updated_at` TIMESTAMP DEFAULT null
 );
 
 CREATE TABLE `order_detail` (
-  `order_id` VARCHAR(255),
-  `product_id` VARCHAR(255),
-  `quantity` INT NOT NULL,
+  `order_id` int,
+  `product_id` int,
+  `quantity` int NOT NULL,
   PRIMARY KEY (`order_id`, `product_id`)
 );
 
 ALTER TABLE `order` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
 
-ALTER TABLE `order_detail` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`);
+ALTER TABLE `order_detail` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
 
-ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
+ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 ALTER TABLE `order_detail` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
