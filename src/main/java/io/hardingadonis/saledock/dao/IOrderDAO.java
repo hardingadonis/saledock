091bdb3c9db59c6506635fbf22c5dd7be75b1280
Minh Vương <hardingadonis@gmail.com>
2024-01-23 20:24:43 +0700

    Add method to interface DAO for APIs

--- src/main/java/io/hardingadonis/saledock/dao/IOrderDAO.java
+++ src/main/java/io/hardingadonis/saledock/dao/IOrderDAO.java
@@ -7,4 +7,8 @@ public interface IOrderDAO extends IDAO<Order> {
     default Long countOrderInProcess() {
         return 0L;
     }
+
+    public String statisticBySold(Integer duration);
+    
+    public String statisticByStatus(Integer duration);
 }
--- src/main/java/io/hardingadonis/saledock/dao/impl/OrderDAOImpl.java
+++ src/main/java/io/hardingadonis/saledock/dao/impl/OrderDAOImpl.java
@@ -6,6 +6,7 @@ import io.hardingadonis.saledock.utils.*;
 import java.sql.*;
 import java.util.*;
 import org.hibernate.*;
+import org.json.simple.*;
 
 public class OrderDAOImpl implements IOrderDAO {
 
@@ -62,7 +63,7 @@ public class OrderDAOImpl implements IOrderDAO {
 
         return count;
     }
-    
+
     @Override
     public Long countOrderInProcess() {
         Long count = 0L;
@@ -84,5 +85,73 @@ public class OrderDAOImpl implements IOrderDAO {
         }
 
         return count;
+    }
+
+    @Override
+    public String statisticBySold(Integer duration) {
+        JSONObject json = new JSONObject();
+
+        try {
+            Connection conn = Singleton.dbContext.getConnection();
+
+            PreparedStatement smt = conn.prepareStatement("SELECT COUNT(*), DATE_FORMAT(SUBSTR(`created_at`, 1, 10), 'Tháng %m năm %Y') FROM `order` WHERE `created_at` BETWEEN DATE_SUB(CURDATE(), INTERVAL ? MONTH) AND CURDATE() GROUP BY SUBSTR(`created_at`, 1, 7)");
+            smt.setInt(1, duration);
+
+            ResultSet rs = smt.executeQuery();
+
+            JSONArray labels = new JSONArray();
+            JSONArray dataset = new JSONArray();
+
+            while (rs.next()) {
+                Long data = rs.getLong(1);
+                String label = rs.getString(2);
+                
+                labels.add(label);
+                dataset.add(data);
+            }
+
+            json.put("labels", labels);
+            json.put("dataset", dataset);
+
+            Singleton.dbContext.closeConnection(conn);
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        return json.toJSONString();
+    }
+    
+    @Override
+    public String statisticByStatus(Integer duration) {
+        JSONObject json = new JSONObject();
+
+        try {
+            Connection conn = Singleton.dbContext.getConnection();
+            
+            PreparedStatement smt = conn.prepareStatement("SELECT COUNT(*), CASE WHEN `status` = 'PENDING' THEN 'Đang xử lý' WHEN `status` = 'SHIPPING' THEN 'Đang giao hàng' WHEN `status` = 'DONE' THEN 'Đã hoàn thành' WHEN `status` = 'CANCELLED' THEN 'Đã hủy' END FROM `order` WHERE `created_at` BETWEEN DATE_SUB(CURDATE(), INTERVAL ? MONTH) AND CURDATE() GROUP BY `status`");
+            smt.setInt(1, duration);
+
+            ResultSet rs = smt.executeQuery();
+
+            JSONArray labels = new JSONArray();
+            JSONArray dataset = new JSONArray();
+
+            while (rs.next()) {
+                Long data = rs.getLong(1);
+                String label = rs.getString(2);
+                
+                labels.add(label);
+                dataset.add(data);
+            }
+
+            json.put("labels", labels);
+            json.put("dataset", dataset);
+
+            Singleton.dbContext.closeConnection(conn);
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        return json.toJSONString();
     }
 }
