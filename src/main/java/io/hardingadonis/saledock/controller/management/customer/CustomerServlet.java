8af69b678982eb0c8cb5dd58c2283a13dcee4494
Restyled.io <commits@restyled.io>
2024-01-27 14:18:55 +0000

    Restyled by clang-format

--- src/main/java/io/hardingadonis/saledock/controller/management/customer/CustomerServlet.java
+++ src/main/java/io/hardingadonis/saledock/controller/management/customer/CustomerServlet.java
@@ -2,42 +2,36 @@ package io.hardingadonis.saledock.controller.management.customer;
 
 import io.hardingadonis.saledock.dao.impl.CustomerDAOImpl;
 import io.hardingadonis.saledock.model.Customer;
-import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
+import java.io.*;
 import java.util.List;
 
 @WebServlet(name = "CustomerServlet", urlPatterns = {"/customer"})
 public class CustomerServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
+    request.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html; charset=UTF-8");
 
-        HttpSession session = request.getSession();
+    HttpSession session = request.getSession();
 
+    CustomerDAOImpl customerDAO = new CustomerDAOImpl();
 
-        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+    List<Customer> listC = customerDAO.getAll();
 
+    session.setAttribute("listC", listC);
 
-        List<Customer> listC = customerDAO.getAll();
+    request.getRequestDispatcher("/view/jsp/management/customer/customer.jsp")
+        .forward(request, response);
+  }
 
-
-        session.setAttribute("listC", listC);
-
-
-        request.getRequestDispatcher("/view/jsp/management/customer/customer.jsp").forward(request, response);
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    }
+  @Override
+  protected void doPost(HttpServletRequest request,
+                        HttpServletResponse response)
+      throws ServletException, IOException {}
 }
