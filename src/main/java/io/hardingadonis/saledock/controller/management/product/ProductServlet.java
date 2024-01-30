b260fadb21ae7bc2abf803d929d1aeb3d64c843d
Son <ntson0508@gmail.com>
2024-01-30 18:23:25 +0700

    product-page-ver1

--- src/main/java/io/hardingadonis/saledock/controller/management/product/ProductServlet.java
+++ src/main/java/io/hardingadonis/saledock/controller/management/product/ProductServlet.java
@@ -1,9 +1,12 @@
 package io.hardingadonis.saledock.controller.management.product;
 
+import io.hardingadonis.saledock.model.Product;
+import io.hardingadonis.saledock.utils.Singleton;
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
+import java.util.List;
 
 @WebServlet(name = "ProductServlet", urlPatterns = {"/product"})
 public class ProductServlet extends HttpServlet {
@@ -11,6 +14,14 @@ public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        List<Product> products = Singleton.productDAO.getAll();
+        request.setAttribute("products", products);
+        request.setAttribute("page", "product");
+
+        request.getRequestDispatcher("/view/jsp/management/product/product.jsp").forward(request, response);
     }
 
     @Override
