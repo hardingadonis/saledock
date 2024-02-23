6d994b8b05223314c3f60532a97fc382779d4620
linhlm0210 <lammylinh7012@gmail.com>
2024-02-23 13:52:23 +0700

    linh-update-order

--- src/main/java/io/hardingadonis/saledock/controller/management/order/UpdateOrderServlet.java
+++ src/main/java/io/hardingadonis/saledock/controller/management/order/UpdateOrderServlet.java
@@ -1,10 +1,12 @@
 package io.hardingadonis.saledock.controller.management.order;
 
+import io.hardingadonis.saledock.model.*;
+import io.hardingadonis.saledock.utils.*;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
-
 import java.io.*;
+import java.util.Optional;
 
 @WebServlet(name = "UpdateOrderServlet", urlPatterns = {"/update-order"})
 public class UpdateOrderServlet extends HttpServlet {
@@ -15,10 +17,52 @@ public class UpdateOrderServlet extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
 
         request.setAttribute("page", "order");
+
+        String id = request.getParameter("id");
+        if (id == null) {
+            response.sendError(404);
+            return;
+        }
+
+        Integer id_order = Integer.parseInt(id);
+        Optional<Order> order = Singleton.orderDAO.getByID(id_order);
+
+        if (order.isPresent()) {
+            var ord = order.get();
+            request.setAttribute("ord", ord);
+
+            request.getRequestDispatcher("/view/jsp/management/order/update-order.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/order");
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String ID = request.getParameter("id");
+        Order order = Singleton.orderDAO.getByID(Integer.valueOf(ID)).orElse(null);
+        
+        
+        String status = request.getParameter("status");
+        
+        
+        if (status != null && !status.equals(order.getStatus()) ) {
+           
+            order.setStatus(Order.Status.valueOf(status));
+            Singleton.orderDAO.save(order);
+
+             response.sendRedirect("./order");
+            return;
+        }
+        else {
+            response.sendRedirect("./order");
+            return;
+        }
+        
     }
+    
+    
+    
+    
 }
